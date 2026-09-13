2ac3a4fe5271ec2e3fa91ad2e5b3b2ac45e22b25
Nildae80 <nildae80@gmail.com>
2026-09-12 23:15:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,19 @@ if modulos == "Home":
   st.write("Información general del estudiante: ")
   st.write("Año: 2026")
   st.write("Breve descripción del proyecto: Este proyecto representa la primera aplicación práctica del módulo y permitirá evidenciar el uso de estructuras de datos, widgets, funciones, clases y lógica de programación en una interfaz interactiva.")
-  st.markdown("Tecnologías utilizadas: ")
+  st.write("Tecnologías utilizadas: ")
 
 elif modulos == "Ejercicio 1":
   st.write("Te encuentas en la ventana de ejercicio 1")
+  st.markdown("En este ejercicio se deberá desarrollar un pequeño módulo para registrar movimientos financieros en una lista vacía.")
+  
+  concepto = st.text_input("Ingresa el concepto del movimiento")
+  tipoMovimiento = st.selectbox("Selecciones el tipo de movimiento",["Ingreso","Gasto"])
+  valor = st.number_input("Ingresa el importe del movimiento", value=0)
+  
+  movimiento = list(range(concepto, tipoMovimiento, valor)
+
+  st.write(movimiento)
 
 elif modulos == "Ejercicio 2":
   st.write("Te encuentas en la ventana de ejercicio 2")
