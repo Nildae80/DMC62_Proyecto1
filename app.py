5b359c4857b4fe86d0f90ac297be5aa16f28300e
Nildae80 <nildae80@gmail.com>
2026-09-12 23:00:42 -0500

    Refactor Streamlit app module selection logic

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 import streamlit as st
 
-st.title("PROYECTO 1")
-st.title("APLICACIÓN EN STREAMLIT")
-
-st.header("Elaborado por: Nilda Echevarria")
-st.subheader("Modulo 1")
-st.write("Información general del estudiante: ")
-st.write("Año: 2026")
-st.write("Breve descripción del proyecto: Este proyecto representa la primera aplicación práctica del módulo y permitirá evidenciar el uso de estructuras de datos, widgets, funciones, clases y lógica de programación en una interfaz interactiva.")
-st.markdown("Tecnologías utilizadas: ")
-st.sidebar.title("Menú lateral")
-
 modulos = st.sidebar.selectbox("Selecciones el modulo",["Home","Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
 
-if modulos == "Ejercicio 1":
+if modulos == "Homa"
+  st.title("PROYECTO 1")
+  st.title("APLICACIÓN EN STREAMLIT")
+
+  st.header("Elaborado por: Nilda Echevarria")
+  st.subheader("Modulo 1")
+  st.write("Información general del estudiante: ")
+  st.write("Año: 2026")
+  st.write("Breve descripción del proyecto: Este proyecto representa la primera aplicación práctica del módulo y permitirá evidenciar el uso de estructuras de datos, widgets, funciones, clases y lógica de programación en una interfaz interactiva.")
+  st.markdown("Tecnologías utilizadas: ")
+  st.sidebar.title("Menú lateral")
+
+elif modulos == "Ejercicio 1":
   sr.write("Te encuentas en la ventana de ejercicio 1")
 
 elif modulos == "Ejercicio 2":
