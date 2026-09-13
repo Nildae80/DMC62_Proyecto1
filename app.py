3d429d5aa0fa47cf9105cc4382b006674cfc47e0
Nildae80 <nildae80@gmail.com>
2026-09-12 22:53:09 -0500

    Change write to markdown for technologies used

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.subheader("Modulo 1")
 st.write("Información general del estudiante: ")
 st.write("Año: 2026")
 st.write("Breve descripción del proyecto: Este proyecto representa la primera aplicación práctica del módulo y permitirá evidenciar el uso de estructuras de datos, widgets, funciones, clases y lógica de programación en una interfaz interactiva.")
-st.write("Tecnologías utilizadas: ")
+st.markdown("Tecnologías utilizadas: ")
 st.sidebar.title("Menú lateral")
 
 modulos = st.sidebar.selectbox("Selecciones el modulo",["Home","Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
