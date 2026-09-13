043a8ff9e6fc6fbbee04d2617c6495ebda6a3e3c
Nildae80 <nildae80@gmail.com>
2026-09-12 22:50:51 -0500

    Update subheader and move selectbox to sidebar

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ st.title("PROYECTO 1")
 st.title("APLICACIÓN EN STREAMLIT")
 
 st.header("Elaborado por: Nilda Echevarria")
-st.subheader("Nombre del módulo: Modulo 1")
+st.subheader("Modulo 1")
 st.write("Información general del estudiante: ")
 st.write("Año: 2026")
 st.write("Breve descripción del proyecto: Este proyecto representa la primera aplicación práctica del módulo y permitirá evidenciar el uso de estructuras de datos, widgets, funciones, clases y lógica de programación en una interfaz interactiva.")
 st.write("Tecnologías utilizadas: ")
 st.sidebar.title("Menú lateral")
 
-#modulos = st.selectbox("Selecciones el modulo",["Home","Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
+modulos = st.sidebar.selectbox("Selecciones el modulo",["Home","Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
