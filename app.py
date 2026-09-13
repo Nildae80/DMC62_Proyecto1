9cbe3e9a925e1c886e0b24745420b907904ff49e
Nildae80 <nildae80@gmail.com>
2026-09-12 22:35:41 -0500

    Update sidebar title and add project details

--- app.py
+++ app.py
@@ -2,4 +2,16 @@ import streamlit as st
 
 st.title("PROYECTO 1 – APLICACIÓN EN STREAMLIT")
 
-st.sidebar.title("Home")
+st.sidebar.title("Menú lateral")}
+
+st.write("Elaboraro por: Nilda Echevarria")
+
+st.write("Nombre del módulo")
+
+st.write("Información general del estudiante")
+
+st.write("Año")
+
+st.write("Breve descripción del proyecto")
+
+st.write("Tecnologías utilizadas")
