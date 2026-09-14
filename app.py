9c390694ec1d9a2df95d0ea315ae666594a44a69
Nildae80 <nildae80@gmail.com>
2026-09-14 17:18:26 -0500

    Disable income and expense total display
    Comment out income and expense total calculations.

--- app.py
+++ app.py
@@ -42,15 +42,16 @@ elif modulos == "Ejercicio 1":
     mov[2] if mov[1] == "Ingreso" else -mov[2] 
     for mov in st.session_state.movimientos
   )
-  if mov[1] == "Ingreso":
-    ingresos_total = sum(mov[2] for mov in st.session_state.movimientos)
-  else:
-    gastos_total = sum(mov[2] for mov in st.session_state.movimientos)
+  
+  #if mov[1] == "Ingreso":
+  #  ingresos_total = sum(mov[2] for mov in st.session_state.movimientos)
+  #else:
+  #  gastos_total = sum(mov[2] for mov in st.session_state.movimientos)
     
   st.subheader("Elementos guardados:")
   st.write(st.session_state.movimientos)
-  st.write("Ingresos total: ", f"{ingresos_total:.2f}")
-  st.write("Gastos total: ", f"{gastos_total:.2f}")
+  #st.write("Ingresos total: ", f"{ingresos_total:.2f}")
+  #st.write("Gastos total: ", f"{gastos_total:.2f}")
   st.write("Saldo total: ", f"{total:.2f}")
 
 
