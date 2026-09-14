0b4149a1de104512ca825d4e15609aaa642751b7
Nildae80 <nildae80@gmail.com>
2026-09-14 16:44:16 -0500

    Fix formatting of total income, expenses, and balance display

--- app.py
+++ app.py
@@ -45,9 +45,9 @@ elif modulos == "Ejercicio 1":
 
   st.subheader("Elementos guardados:")
   st.write(st.session_state.movimientos)
-  st.write("Ingresos total: " ingresos_total)
-  st.write("Gastos total: " gastos_total)
-  st.write("Saldo total: " ingresos_total - gastos_total)
+  st.write("Ingresos total: ", ingresos_total)
+  st.write("Gastos total: ", gastos_total)
+  st.write("Saldo total: ", ingresos_total - gastos_total)
 
 
 elif modulos == "Ejercicio 2":
