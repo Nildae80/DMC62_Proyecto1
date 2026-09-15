3fe797eb74c1393f5cc28fa72d4e791b53603164
Nildae80 <nildae80@gmail.com>
2026-09-14 19:47:02 -0500

    Modify input constraints for movimiento entry
    Updated the number input to require a minimum value of 0.01 and set the selectbox index to None.

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ elif modulos == "Ejercicio 1":
     st.session_state.movimientos = []
     
   concepto = st.text_input("Ingresa el concepto del movimiento")
-  tipo_Movimiento = st.selectbox("Selecciones el tipo de movimiento",["Ingreso","Gasto"])
-  importe = float(st.number_input("Ingresa el importe del movimiento S/ ", value=0.00, min_value=0.0, step=0.5, format="%.2f"))
+  tipo_Movimiento = st.selectbox("Selecciones el tipo de movimiento",["Ingreso","Gasto"],index=None)
+  importe = float(st.number_input("Ingresa el importe del movimiento S/ ", value=0.00, min_value=0.01, step=0.5, format="%.2f"))
   
   if st.button("Guardar"):
     ingresos_total = 0
