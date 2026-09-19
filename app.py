1295cc2e6efab3b539d19faa6a82e26b3158de39
Nildae80 <nildae80@gmail.com>
2026-09-18 19:07:25 -0500

    Fix variable names in file transfer time calculation

--- app.py
+++ app.py
@@ -175,7 +175,7 @@ elif modulos == "Ejercicio 3":
   tamano_archivo = float(st.number_input("Ingresa el tamaño del archivo (MB) ", value=0.00, min_value=0.0, step=0.1, format="%.2f"))
   velocidad = float(st.number_input("Ingresa la velocidad de transferencia (MBPS) ", value=0.00, min_value=0.0, step=0.1, format="%.2f"))
 
-  resultado_tiempo =  lf.calcular_tiempo_transferencia_archivo(tamano,velocidad)
+  resultado_tiempo =  lf.calcular_tiempo_transferencia_archivo(tamano_archivo,velocidad)
   st.write("El resultado de tu valor futuro de inversion es: ",round(resultado_tiempo,2))
   
 
