0455ce4e598118ebff19422821ba13de10e39695
Nildae80 <nildae80@gmail.com>
2026-09-19 18:58:36 -0500

    Add import for Servidor class in app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import libreria_funciones_proyecto1 as lf
+
 from libreria_clases_proyecto1 import Servidor
 
 st.sidebar.title("Especialización en Python for Analytics")
