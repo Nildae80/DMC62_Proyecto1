f554856daed4f15636ca4993e2262e0bb73b98c1
Nildae80 <nildae80@gmail.com>
2026-09-18 22:21:45 -0500

    Fix output formatting for transfer time result

--- app.py
+++ app.py
@@ -176,10 +176,8 @@ elif modulos == "Ejercicio 3":
   velocidad = float(st.number_input("Ingresa la velocidad de transferencia (MBPS) ", value=0.00, min_value=0.0, step=0.1, format="%.2f"))
 
   resultado_tiempo =  lf.calcular_tiempo_transferencia_archivo(tamano_archivo,velocidad)
-  st.write(f"El resultado de tu valor futuro de inversion es: ",round(resultado_tiempo,2))
-          "tiempo_segundos": round(tiempo_segundos, 2),
-        "tiempo_minutos": round(tiempo_minutos, 2)
-  
+  st.write(f"El resultado de tu valor futuro de inversion es: {resultado_tiempo[tiempo_minutos]} : {resultado_tiempo[tiempo_segundos]}")
+
 
 else:
   st.header("Te encuentas en la ventana de ejercicio 4")
