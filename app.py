1221fd226b7e1463824685bc9dc88f4e088b0268
Nildae80 <nildae80@gmail.com>
2026-09-18 23:49:31 -0500

    Update default values for file size and speed inputs

--- app.py
+++ app.py
@@ -183,8 +183,8 @@ elif modulos == "Ejercicio 3":
   tipo_Funcion = st.selectbox("Seleccione el tipo de función",["Calcular tiempo de transferencia de archivo", "Otro"],index=None,placeholder="Seleccione tipo de movimiento...",)
   
   if tipo_Funcion == "Calcular tiempo de transferencia de archivo": 
-    tamano_archivo = float(st.number_input("Ingresa el tamaño del archivo (MB)",value=0.00,min_value=0.1,step=0.1,format="%.2f",))
-    velocidad = float(st.number_input("Ingresa la velocidad de transferencia (MBPS)",value=0.00,min_value=0.1,step=0.1,format="%.2f",))
+    tamano_archivo = float(st.number_input("Ingresa el tamaño del archivo (MB)",value=0.10,min_value=0.1,step=0.1,format="%.2f",))
+    velocidad = float(st.number_input("Ingresa la velocidad de transferencia (MBPS)",value=0.10,min_value=0.1,step=0.1,format="%.2f",))
   
     if st.button("Ejecutar", type="primary"):
       if velocidad <= 0 or tamano_archivo <= 0:
