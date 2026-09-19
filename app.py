f9dc60bccbe4346c51d46ac573a15b9842d6ef92
Nildae80 <nildae80@gmail.com>
2026-09-19 17:15:52 -0500

    Import Servidor from libreria_clases_proyecto1

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import libreria_funciones_proyecto1 as lf
-import librería_clases_proyecto1 as lc
+from libreria_clases_proyecto1 import Servidor
 
 st.sidebar.title("Especialización en Python for Analytics")
 imagen = st.sidebar.image("Python_logo.png", width=200)
