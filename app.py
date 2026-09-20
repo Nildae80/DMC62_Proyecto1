a215beac0c2b7eb15929a2d3a538772a1396509a
Nildae80 <nildae80@gmail.com>
2026-09-19 19:03:43 -0500

    Change minimum value for total storage input to 0

--- app.py
+++ app.py
@@ -253,7 +253,7 @@ else:
       nombre = st.text_input("Nombre del Servidor")  
       tiempo_total = st.number_input("Tiempo Total de Operación (horas)",min_value=0.0,value=0.0,step=10.0,format="%.2f",)
       tiempo_caida = st.number_input("Tiempo de Caída (horas)",min_value=0.0,value=0.0,step=0.5,format="%.2f",)
-      alm_total = st.number_input("Almacenamiento Total (GB)",min_value=1.0,value=0.0,step=50.0,format="%.2f",)
+      alm_total = st.number_input("Almacenamiento Total (GB)",min_value=0.0,value=0.0,step=50.0,format="%.2f",)
       alm_usado = st.number_input("Almacenamiento Usado (GB)",min_value=0.0,value=0.0,step=10.0,format="%.2f",)
   
       btn_guardar = st.form_submit_button("Guardar Servidor", type="primary")
