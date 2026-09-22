ea8e80bdd10f9fab296b249a34719fd184c057f1
Nildae80 <nildae80@gmail.com>
2026-09-21 22:29:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -222,11 +222,10 @@ elif modulos == "Ejercicio 3":
 
     with st.form("form_funcion", clear_on_submit=True):
         tipo_Funcion = st.selectbox("Seleccione el tipo de función",["Calcular tiempo de transferencia de archivo", "Otro"],index=None,placeholder="Seleccione tipo de movimiento...",)
-    
-        if tipo_Funcion == "Calcular tiempo de transferencia de archivo": 
-            tamano_archivo = float(st.number_input("Ingresa el tamaño del archivo (MB)",value=0.00,min_value=0.0,step=0.1,format="%.2f",))
-            velocidad = float(st.number_input("Ingresa la velocidad de transferencia (MBPS)",value=0.00,min_value=0.0,step=0.1,format="%.2f",))
-            btn_guardar = st.form_submit_button("Guardar ")
+        if tipo_Funcion == "Calcular tiempo de transferencia de archivo": 
+            tamano_archivo = float(st.number_input("Ingresa el tamaño del archivo (MB)",value=0.00,min_value=0.0,step=0.1,format="%.2f",))
+            velocidad = float(st.number_input("Ingresa la velocidad de transferencia (MBPS)",value=0.00,min_value=0.0,step=0.1,format="%.2f",))
+            btn_guardar = st.form_submit_button("Guardar ")
     
             if btn_guardar:
                 if velocidad <= 0 or tamano_archivo <= 0:
