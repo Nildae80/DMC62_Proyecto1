Fix indentation and error handling in file transfer calculation

app.py
@@ -226,15 +226,13 @@ elif modulos == "Ejercicio 3":
             tamano_archivo = float(st.number_input("Ingresa el tamaño del archivo (MB)",value=0.00,min_value=0.0,step=0.1,format="%.2f",))
             velocidad = float(st.number_input("Ingresa la velocidad de transferencia (MBPS)",value=0.00,min_value=0.0,step=0.1,format="%.2f",))
             btn_guardar = st.form_submit_button("Guardar ")
-    
-            if btn_guardar:
+            
+            if btn_guardar:
                 if velocidad <= 0 or tamano_archivo <= 0:
-                    st.error("El tamaño del archivo y la velocidad deben ser mayores a 0.")
-            else:
-                resultado_tiempo = lf.calcular_tiempo_transferencia_archivo(tamano_archivo, velocidad)
-      
-                minutos = resultado_tiempo["tiempo_minutos"]
-                segundos = resultado_tiempo["tiempo_segundos"]
+                    st.error("El tamaño del archivo y la velocidad deben ser mayores a 0.")
+                else:
+                    minutos = resultado_tiempo["tiempo_minutos"]
+                    segundos = resultado_tiempo["tiempo_segundos"]
   
             if "tiempo" not in st.session_state or st.session_state.tiempo.shape[1] != 4:
               st.session_state.tiempo = np.empty((0, 4), dtype=object)