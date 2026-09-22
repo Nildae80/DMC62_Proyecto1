1153be9c87ed985afd9a4d9a4372d4dac251b775
Nildae80 <nildae80@gmail.com>
2026-09-21 22:47:53 -0500

    Add lightning bolt emoji to subheader in app.py

--- app.py
+++ app.py
@@ -252,7 +252,7 @@ elif modulos == "Ejercicio 3":
     
     # Mostrar los datos guardados en una tabla
     if st.session_state.tiempo.shape[0] > 0:
-        st.subheader(" Tabla histórica de resultados obtenidos")
+        st.subheader("⚡ Tabla histórica de resultados obtenidos")
         df_mostrar = pd.DataFrame(st.session_state.tiempo,columns=["Tamaño (MB)","Velocidad (MBPS)","Tiempo en Minutos", "Tiempo en Segundos"],)
         
         # 3. Formato corregido para NumberColumn
