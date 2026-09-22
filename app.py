6b1a7c975aa00235beabe7107fa49f0a619637f4
Nildae80 <nildae80@gmail.com>
2026-09-21 22:18:03 -0500

    Fix variable name in session_state for Ejercicio 3

--- app.py
+++ app.py
@@ -216,8 +216,7 @@ elif modulos == "Ejercicio 2":
 elif modulos == "Ejercicio 3":
     st.header("Te encuentras en la ventana del Ejercicio 3")
     st.write("En este ejercicio se usará funciones desde una librería externa.")
-  
-    # 1. Nombre de variable corregido en el session_state (2 columnas)
+    # 1. Nombre de variable corregido en el session_state (2 columnas)
     if "tiempo" not in st.session_state:
         st.session_state.tiempo = np.empty((0, 2), dtype=object)
   
